4bc19021dc44ad028edffda4812c08fc53270381
erkan.esmer <erkanesmer@gmail.com>
2025-03-31 02:01:41 +0300

    Main Menu added

--- machine/usr/local/safesamba/mainmenu.sh
+++ machine/usr/local/safesamba/mainmenu.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+function show_menu(){
+date
+echo "   |--------------------------------------------------------------------|"
+echo "   | :::.. SafeSamba SecureFileserver ..:::                             |"
+echo "   |--------------------------------------------------------------------|"
+echo "   | #  USer Man.  | Share Man.      | Logs/Reports                     |"
+echo "   | -------------------------------------------------------------------|"
+echo "   | 1.Add User    | 11.New Share    | 20.View Logs                     |"
+echo "   | 2.Delete User | 12.Remove Share | 21.Quarantine Files              |"
+echo "   | 3.User List   | 13.Share List   | 22.Scan Service Status           |"
+echo "   |--------------------------------------------------------------------|"
+echo "   | 99.Exit                                                            |"
+echo "   |--------------------------------------------------------------------|"
+}
